f7cb844291408de47e3d1a0c4a578b733d9661c2
Bobi <bobi_tomov@outlook.com>
2022-03-09 00:42:28 +0200

    r

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -40,4 +41,7 @@ mongoose
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+  });
 }
