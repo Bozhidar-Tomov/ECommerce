a2e3ccf9d84fb95c04ea69f476d6b84a8e353a6c
Bobi <bobi_tomov@outlook.com>
2022-03-18 18:34:31 +0200

    addedterde

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const { fetchAllProducts, fetchOneProduct } = require("../controllers/product.js");
 
-router.get("/app", fetchAllProducts);
+router.get("/", fetchAllProducts);
 router.get("/:id", fetchOneProduct);
 
 module.exports = router;
